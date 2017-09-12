143478a4c0d59a9756285a27b2ed28b149e56eb0
seravifer <sergiavilafernandez@gmail.com>
2017-09-12 21:07:39 +0200

    Some fixies

--- content.js
+++ content.js
@@ -1,62 +1,25 @@
 $(document).ready(function() {
     
-    $('#selectOrigen option[value="' + Cookies.get('cookieOrigen') + '"]').attr("selected", "selected");
-    $('#selectDestination option[value="' + Cookies.get('cookieDestination') + '"]').attr("selected", "selected");
+    $('#selectOrigen option[value="' + localStorage.getItem("cookieOrigen") + '"]').prop('selected', true);
+    $('#selectDestination option[value="' + localStorage.getItem('cookieDestination') + '"]').prop('selected', true);
     $('#date').val(getDay());
     $('#hora').val(getTime());
     
-    function parseData(data) {
-        var table = $(data).find('table');
-        var info = $(data).find('li');
-        $('#info').append(info.eq(4).text().substring(34, 50));
-        $('#info').append('<br>Zona: ' + info.eq(5).text().substring(66, 69));
-        
-        table.find('tr').each(function (i, row) {
-            $(this).find('td').each(function(j, cell) {
-                if (j != 0 && cell.innerHTML != "---") {
-                    $('#timetable').append('<div class="time">Salida a las ' + cell.innerHTML + 'h dentro de ' + timeLeft(cell.innerHTML) + '</div>');
-                    //console.log(cell.innerHTML);
-                }
-            });
-        });
-    }
-    
-    function timeLeft(nextTime) {
-        var date = new Date();
-        var actualHour = (date.getHours() < 10 ? '0' : '') + date.getHours();
-        var actualMin = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-        var nextHour = nextTime.substring(0,2);
-        var nextMin = nextTime.substring(3,5);
-        
-        var total = ((nextHour * 60) + nextMin) - ((actualHour * 60) + actualMin);
-        
-        return (total / 60 / 100 | 0) + ':' + (total % 60 < 10 ? '0' : '') + total % 60;
-    }
-    
-    function getDay() {
-        var today = new Date();
-        var dd = (today.getDate() < 10 ? '0' : '') + today.getDate();
-        var mm = ((today.getMonth() + 1) < 10 ? '0' : '') + (+today.getMonth() + +1); // January is 0
-        var yyyy = today.getFullYear();
-        
-        return dd + '/' + mm + '/' + yyyy;
-    }
-    
-    function getTime() {
-        var date = new Date();
-        var hh = (date.getHours() < 10 ? '0' : '') + date.getHours();
-        var mm = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-        
-        return hh + ':' + mm;
-    }
+    $('#selectOrigen').change(function() {
+        localStorage.setItem("cookieOrigen", $(this).val());
+    });
+
+    $('#selectDestination').change(function() {
+        localStorage.setItem("cookieDestination",  $(this).val());
+    });
     
     $('#invert').click(function() {
-        /*var aux = $('#selectOrigen option:selected').val();
-        $('#selectOrigen option[value="' + $('#selectDestination option:selected').val() + '"]').attr("selected", "selected");
-        $('#selectDestination option[value="' + aux + '"]').attr("selected", "selected");/*/
         var aux = $('#selectOrigen').val();
         $('#selectOrigen').val($('#selectDestination option:selected').val());
         $('#selectDestination').val(aux);
+        
+        localStorage.setItem("cookieDestination",  $(this).val());
+        localStorage.setItem("cookieOrigen", $(this).val());
     })
 
     $('#form').submit(function(event) {
@@ -73,12 +36,10 @@ $(document).ready(function() {
         };
 
         $.ajax({
-            url         : 'http://www.metrovalencia.es/horarios.mobi.php',
-            type        : 'POST',
+            url         : 'https://cors.now.sh/http://www.metrovalencia.es/horarios.mobi.php',
+            type        : 'GET',
             data        : formData,
-            encode      : true,
-            dataType    : 'html'
-            
+            encode      : true
         }).done(function(data) {
             parseData(data);
             window.location.href = "#schedule";
@@ -90,12 +51,49 @@ $(document).ready(function() {
         
     });
     
-    $('#selectOrigen').change(function() {
-        Cookies.set('cookieOrigen', $(this).val(), {expires: 30});
-    });
-    
-    $('#selectDestination').change(function() {
-        Cookies.set('cookieDestination', $(this).val(), {expires: 30});
-    });
+    function parseData(data) {
+        var table = $(data).find('table');
+        var info = $(data).find('li');
+        $('#info').append(info.eq(4).text().substring(34, 50));
+        $('#info').append('<br>Zona: ' + info.eq(5).text().substring(66, 69));
+
+        table.find('tr').each(function (i, row) {
+            $(this).find('td').each(function(j, cell) {
+                if (j != 0 && cell.innerHTML != "---") {
+                    $('#timetable').append('<div class="time">Salida a las ' + cell.innerHTML + 'h dentro de ' + timeLeft(cell.innerHTML) + '</div>');
+                    //console.log(cell.innerHTML);
+                }
+            });
+        });
+    }
+
+    function timeLeft(nextTime) {
+        var date = new Date();
+        var actualHour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+        var actualMin = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+        var nextHour = nextTime.substring(0,2);
+        var nextMin = nextTime.substring(3,5);
+
+        var total = ((nextHour * 60) + nextMin) - ((actualHour * 60) + actualMin);
+
+        return (total / 60 / 100 | 0) + ':' + (total % 60 < 10 ? '0' : '') + total % 60;
+    }
+
+    function getDay() {
+        var today = new Date();
+        var dd = (today.getDate() < 10 ? '0' : '') + today.getDate();
+        var mm = ((today.getMonth() + 1) < 10 ? '0' : '') + (+today.getMonth() + +1); // January is 0
+        var yyyy = today.getFullYear();
+
+        return dd + '/' + mm + '/' + yyyy;
+    }
+
+    function getTime() {
+        var date = new Date();
+        var hh = (date.getHours() < 10 ? '0' : '') + date.getHours();
+        var mm = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+
+        return hh + ':' + mm;
+    }
 
 });
